6d1ea9253549d6abad2263f54bed74d4fa63ec7f
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-06-12 02:25:55 +0530

    change

--- Force/a.cpp
+++ Force/a.cpp
@@ -30,7 +30,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout << fixed << setprecision(9);
-    int t = 1; cin >> t;
+    int t = 1; //cin >> t;
     for (int _i = 1; _i <= t; _i++) {
         //cout << "Case #" << _i << ": ";
         solve();
