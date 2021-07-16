222dec4a78300221002d6163583a2ff7b58cfa20
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-07-16 15:26:27 +0530

    change

--- Force/a.cpp
+++ Force/a.cpp
@@ -15,7 +15,7 @@ template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(
 template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
 
 const lli INF = 1e18, MOD = 1e9+7;
-const int N = 2e5;
+const int N = 4e5;
 const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
 const string YN[] = {"NO", "YES"};  
 
