2191ba05d8473e81e23a9d46016f9303ea3db2fb
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-09-05 20:00:54 +0530

    change

--- Force/a.cpp
+++ Force/a.cpp
@@ -6,11 +6,11 @@ using namespace std;
 #define vlli vector<lli>
 #define vpii vector<pair<int, int>>
 #define pb push_back 
-template<typename isTrue> void debug(isTrue _a) {cout << _a << " ";}
+template<typename T> void debug(T _a) {cout << _a << " ";}
 template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
-template<typename isTrue> void debug(vector<isTrue> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename isTrue> void debug(multiset<isTrue> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename isTrue> void debug(set<isTrue> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
 
 const lli INF = 1e18, MOD = 1e9+7;
@@ -30,7 +30,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout << fixed << setprecision(9);
-    int t = 1; //cin >> t;
+    int t = 1; cin >> t;
     for (int _i = 1; _i <= t; _i++) {
         //cout << "Case #" << _i << ": ";
         solve();
