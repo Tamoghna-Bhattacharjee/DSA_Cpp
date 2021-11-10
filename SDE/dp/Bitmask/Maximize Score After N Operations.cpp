1485406eec3563d19851d0b7bd382cc03d0b1505
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-11-10 21:08:29 +0530

    change

--- SDE/dp/Bitmask/Maximize Score After N Operations.cpp
+++ SDE/dp/Bitmask/Maximize Score After N Operations.cpp
@@ -1,34 +1,60 @@
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 using namespace std;
+
 #define lli long long int 
 #define ld long double
 #define vi vector<int>
 #define vlli vector<lli>
 #define vpii vector<pair<int, int>>
 #define pb push_back 
+
 template<typename T> void debug(T _a) {cout << _a << " ";}
 template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
 template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
 
-const lli INF = 1e18, MOD = 1e9+7;
-const int N = 5e5;
-const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
-const string YN[] = {"NO", "YES"};   
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-// https://practice.geeksforgeeks.org/problems/longest-alternating-subsequence5951/1
+const lli INF = 1e15, MOD = 1e9+7;
+const int N = 4e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"}; 
+
+// https://leetcode.com/problems/maximize-score-after-n-operations/
+
+vi a;
+vector<vi> dp;
+int n, m;
+
+int f(int k, int mask) {
+    if (k > n) return 0;
+    if (dp[mask][k] != -1) return dp[mask][k];
+    
+    for (int i = 0; i < m-1; i++) {
+        int F1 = (1 << i);
+        if ((mask & F1)) continue;
+        for (int j = i+1; j < m; j++) {
+            int F2 = (1 << j);
+            if (mask & F2) continue;
+            dp[mask][k] = max(dp[mask][k], k * __gcd(a[i], a[j]) + f(k+1, mask+F1+F2));
+        }
+    }
+    return dp[mask][k];
+}
 
 void solve() {
-    int n; cin >> n;
-    int a[n]; for (int &i: a) cin >> i;
-    int inc = 1, dec = 1;
-    for (int i = 1; i < n; i++) {
-		if (a[i] > a[i-1]) inc = dec + 1;
-		else if (a[i] < a[i-1]) dec = inc + 1;
-	}
-	cout << max(inc, dec) << endl;
+    cin >> n;
+    m = 2*n;
+    a = vi(m); for (int &i: a) cin >> i;
+    dp = vector<vi> ((1<<m) + 1, vi(n+1, -1));
+    cout << f(1, 0) << endl;
 }   
   
 int main() {
