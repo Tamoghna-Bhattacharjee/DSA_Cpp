a84a812016de941a0720343722ba406a2c3d8178
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-12-25 18:46:38 +0530

    pb on seg tree

---
+++ Working on ranges/segment tree/EDU/Seg with max Sum.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 998244353;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"}; 
+
+// https://codeforces.com/edu/course/2/lesson/4/2/practice/contest/273278/problem/A
+
+struct node {
+    lli sum, pref, suf, seg;
+    node() {
+        // change
+        sum = pref = suf = seg = 0;
+    }
+    node(lli _sum, lli _pref, lli _suf, lli _seg) {
+        // change
+        sum = _sum; pref = _pref; suf = _suf; seg = _seg;
+    }
+    void merge(node &l, node &r) {
+        // change
+        sum = l.sum + r.sum;
+        pref = max(l.pref, l.sum + r.pref);
+        suf = max(r.suf, r.sum + l.suf);
+        seg = max(max(l.seg, r.seg), l.suf + r.pref);
+    }
+};
+
+struct segTree {
+    lli size;
+    vector<node> st;
+    node identity;
+
+    void init(int x) {
+        size = x;
+        st.resize(4*size+4);
+        identity = node(0,0,0,0); // change
+    }
+
+    void build (int si, int ss, int se, vlli &a) {
+        if (ss > se) return;
+        if (ss == se) {
+            st[si] = node(a[ss], a[ss], a[ss], a[ss]); // change
+            return;
+        }
+        int mid = (ss + se) / 2;
+        build (2*si, ss, mid, a);
+        build (2*si + 1, mid + 1, se, a);
+        st[si].merge(st[2*si], st[2*si+1]);
+    }
+
+    void rupd(int si, int ss, int se, int us, int ue, lli diff) {
+        if (us > se || ue < ss || ss > se) return;
+        if (ss >= us && se <= ue) { 
+            // change
+            st[si].sum += (se - ss + 1) * diff;
+            st[si].pref += (se - ss + 1) * diff;
+            st[si].suf += (se - ss + 1) * diff;
+            st[si].seg += (se - ss + 1) * diff;
+            return;
+        }
+        int mid = (ss + se) / 2;
+        rupd(2*si, ss, mid, us, ue, diff);
+        rupd(2*si + 1, mid + 1, se, us, ue, diff);
+        st[si].merge(st[2*si], st[2*si + 1]);
+    }
+
+    void set(int si, int ss, int se, int ui, int x) {
+        if (ss == se) {
+            st[si] = node(x,x,x,x);
+            return;
+        }
+        int mid = (ss + se) / 2;
+        if (ui <= mid) set(2*si, ss, mid, ui, x);
+        else set(2*si + 1, mid + 1, se, ui, x);
+        st[si].merge(st[2*si], st[2*si + 1]);
+    }
+
+    node qry (int si, int ss, int se, int qs, int qe) {
+        if (qs > se || qe < ss || ss > se) return identity;
+        if (ss >= qs && se <= qe) return st[si];
+        int mid = (ss + se)/2;
+        node l = qry(2*si, ss, mid, qs, qe);
+        node r = qry(2*si+1, mid+1, se, qs, qe);
+        node res; res.merge(l,r);
+        return res;
+    }
+
+    // this functions are actually called by the user
+    void build (vlli &a) {
+        build(1, 1, size, a);
+    }
+
+    void rupd(int us, int ue, lli diff) {
+        if (us > ue) return;
+        rupd(1, 1, size, us, ue, diff);
+    }
+
+    void set(int ui, int x) {
+        set(1,1,size,ui, x);
+    }
+
+    node qry (int qs, int qe) {
+        if (qs > qe) return identity;
+        return qry (1, 1, size, qs, qe);
+    }
+};
+
+void solve() {
+    segTree st;
+    int n, m; cin >> n >> m;
+    vlli a(n+1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    st.init(n);
+    st.build(a);
+    cout << max(0LL, st.qry(1,n).seg) << endl;
+    while (m--) {
+        lli ind, v; cin >> ind >> v; ind++;
+        st.set(ind, v);
+        cout << max(0LL, st.qry(1,n).seg) << endl;
+    }
+}   
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
