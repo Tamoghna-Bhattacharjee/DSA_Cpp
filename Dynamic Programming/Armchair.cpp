9ed4f0347e6bd09abafd6e399ea6fd5cb4c8ee71
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-02-05 16:25:53 +0530

    02/05/2022

---
+++ Dynamic Programming/Armchair.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+
+// https://codeforces.com/contest/1525/problem/D
+
+vlli pos[2];
+vector<vlli> dp; 
+
+lli f(int o, int z) {
+    if (o >= pos[1].size()) return 0;
+    if (z >= pos[0].size()) return INF;
+    if (dp[o][z] != -1) return dp[o][z];
+    // crossing is bad
+    dp[o][z] = min(f(o+1,z+1) + abs(pos[1][o]-pos[0][z]), f(o,z+1));
+    return dp[o][z];
+}
+
+void solve() {
+    int n; cin >> n;
+    vlli a(n); for (auto &i: a) cin >> i;
+    for (lli i = 0; i < n; i++) pos[a[i]].pb(i);
+    dp = vector<vlli>(pos[1].size(), vlli(pos[0].size(), -1));
+    cout << f(0,0) << endl;
+}   
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
