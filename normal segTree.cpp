7e7c5f07b28c7d3f7e523080650723f3e6a7dfe1
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-02-18 18:08:51 +0530

    add

--- normal segTree.cpp
+++ normal segTree.cpp
@@ -71,6 +71,118 @@ lli qry(int si, int ss, int se, int qs, int qe) {
     return l+r;
 }
 
+// WITHOUT LAZY PROGPAGATION
+struct node {
+    lli mx;
+    node() {
+        // change
+        mx = -INF;
+    }
+    node(lli x) {
+        // change
+        mx = x;
+    }
+    void merge(node &l, node &r) {
+        // change
+        mx = max(l.mx, r.mx);
+    }
+};
+
+struct segTree {
+    lli size;
+    vector<node> st;
+    node identity;
+
+    void init(int x) {
+        size = x;
+        st.resize(4*size+4);
+        identity = node(-INF); // change
+    }
+
+    void build (int si, int ss, int se, vlli &a) {
+        if (ss > se) return;
+        if (ss == se) {
+            st[si] = node(a[ss]); // change
+            return;
+        }
+        int mid = (ss + se) / 2;
+        build (2*si, ss, mid, a);
+        build (2*si + 1, mid + 1, se, a);
+        st[si].merge(st[2*si], st[2*si+1]);
+    }
+
+    void set(int si, int ss, int se, int ui, lli x) {
+        if (ss == se) {
+            // change
+            st[si] = node(x);
+            return;
+        }
+        int mid = (ss + se) / 2;
+        if (ui <= mid) set(2*si, ss, mid, ui, x);
+        else set(2*si + 1, mid + 1, se, ui, x);
+        st[si].merge(st[2*si], st[2*si + 1]);
+    }
+
+    void rupd(int si, int ss, int se, int us, int ue, lli diff) {
+        if (us > se || ue < ss || ss > se) return;
+        if (ss >= us && se <= ue) { 
+            // change
+            st[si].mx += (se - ss + 1) * diff;
+            return;
+        }
+        int mid = (ss + se) / 2;
+        rupd(2*si, ss, mid, us, ue, diff);
+        rupd(2*si + 1, mid + 1, se, us, ue, diff);
+        st[si].merge(st[2*si], st[2*si + 1]);
+    }
+
+    node qry (int si, int ss, int se, int qs, int qe) {
+        if (qs > se || qe < ss || ss > se) return identity;
+        if (ss >= qs && se <= qe) return st[si];
+        int mid = (ss + se)/2;
+        node l = qry(2*si, ss, mid, qs, qe);
+        node r = qry(2*si+1, mid+1, se, qs, qe);
+        node res; res.merge(l,r);
+        return res;
+    }
+
+    // lli ptqry(int si, int ss, int se, int qs, int qe, lli x) {
+    //     if (qs > se || qe < ss || ss > se) return -1;
+    //     if (st[si].mx < x) return -1;
+    //     if (ss == se) return ss;
+    //     int mid = (ss + se) / 2;
+    //     lli l = ptqry(2*si, ss, mid, qs, qe, x);
+    //     if (l != -1) return l;
+    //     lli r = ptqry(2*si + 1, mid+1,se, qs, qe, x);
+    //     if (r != -1) return r;
+    //     return -1;
+    // }
+
+    // driver code:
+    void build (vlli &a) {
+        // a must be 1-indexed
+        build(1, 1, size, a);
+    }
+
+    void set(int ui, lli x) {
+        set(1,1,size,ui, x);
+    }
+
+    void rupd(int us, int ue, lli diff) {
+        if (us > ue) return;
+        rupd(1, 1, size, us, ue, diff);
+    }
+
+    node qry (int qs, int qe) {
+        if (qs > qe) return identity;
+        return qry (1, 1, size, qs, qe);
+    }
+
+    // lli ptqry(lli x, int qs, int qe) {
+    //     return ptqry(1,1,size,qs,qe,x);
+    // }
+};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
