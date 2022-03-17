1aaa9a6fc3b51cf1cfe21aeed368d81a81b0fab1
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-03-17 14:42:39 +0530

    add

--- SDE/Bitwise/Check the divisibility of a binaryString.cpp
+++ SDE/Bitwise/Check the divisibility of a binaryString.cpp
@@ -1,54 +1,47 @@
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 using namespace std;
+
 #define lli long long int 
 #define ld long double
 #define vi vector<int>
 #define vlli vector<lli>
 #define vpii vector<pair<int, int>>
 #define pb push_back 
+
 template<typename T> void debug(T _a) {cout << _a << " ";}
 template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
 template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
 template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
 
-const lli INF = 1e18, MOD = 1e9+7;
-const int N = 5e5;
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
 const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
-const string YN[] = {"NO", "YES"};   
+const string YN[] = {"NO", "YES"};
 
-// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
-// https://practice.geeksforgeeks.org/problems/stock-buy-and-sell-1587115621/1/?track=amazon-dynamic-programming&batchId=192#
+// https://www.geeksforgeeks.org/check-divisibility-binary-stream/
 
-vector<vi> buySellstockWithExactDate(vi p, int n) {
-    vector<vector<int>> res;
-    if (n == 1) return res;
-    for (int i = 0; i < n; i++) {
-        int lft = i-1 >= 0? p[i-1]: INT_MAX;
-        int rgt = i+1 < n? p[i+1]: INT_MAX;
-        if (lft >= p[i] && p[i] <= rgt) res.push_back({i,i});
-        lft = i-1 >= 0? p[i-1]: INT_MIN;
-        rgt = i+1 < n? p[i+1]: INT_MIN;
-        if (lft <= p[i] && p[i] >= rgt) {
-            if (res.empty()) continue;
-            else if (res.back()[0] == res.back()[1]) res.back()[1] = i;
-        }
-    }
-    if (res.back()[0] == res.back()[1]) res.pop_back();
-    return res;
-} 
+// Stream of binary number is coming, the task is to tell the number formed 
+// so far is divisible by a given number n.
 
 void solve() {
     int n; cin >> n;
-    vi p(n); for (int &i: p) cin >> i;
-    int cp = INT_MAX, prof = 0;
-    for (int i: p) {
-        cp = min(cp, i-prof);
-        prof = max(prof, i-cp);
+    string s; cin >> s;
+    int rem = 0;
+    for (char i: s) {
+        if (i == '1') rem = rem * 2 + 1;
+        else rem = rem * 2;
+        rem = rem % n;
     }
-    cout << prof << endl;
-    debug(buySellstockWithExactDate(p,n));
+    cout << YN[rem % n == 0] << endl;
 }   
   
 int main() {
@@ -57,7 +50,7 @@ int main() {
         freopen("output.txt","w",stdout);
     #endif
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(0);
     cout << fixed << setprecision(9);
     int t = 1; //cin >> t;
     for (int _i = 1; _i <= t; _i++) {
