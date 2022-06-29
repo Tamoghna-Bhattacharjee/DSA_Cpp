771b89cf61222fc1a86cc5ac7105fc26e53bac25
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-06-29 13:45:20 +0530

    prime palindrome

---
+++ SDE/math/Prime palindrome.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+
+
+// https://leetcode.com/problems/prime-palindrome/
+
+// Given an integer n, return the smallest prime palindrome greater than or equal to n
+// The test cases are generated so that the answer always exists 
+// and is in the range [2, 2 * 10^8]
+
+bool isPal(int n) {
+    int rev = 0, x = n;
+    while (n > 0) {
+        rev = rev * 10 + (n%10);
+        n /= 10;
+    }
+    return rev == x;
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int power(int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n % 2) res *= a;
+        a *= a;
+        n /= 2;
+    }
+    return res;
+}
+
+int primePalindrome(int n) {
+    while (1) {
+        int len = (int) log10(n)+1;
+        if (len % 2 == 0 && len != 2) n = power(10, len);
+        else {
+            if (isPal(n) && isPrime(n)) return n;
+            else n++;
+        }
+    }
+    return 0;
+}
+
+void solve() {
+    int n; cin >> n;
+    cout << primePalindrome(n) << endl;
+}   
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
