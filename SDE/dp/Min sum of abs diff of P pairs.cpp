628cf6c4e7d3e108a8522c5cf91ead3f0f10a4bb
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-08-16 08:39:50 +0530

    Min sum of abs diff of P pairs, P <= N/2

---
+++ SDE/dp/Min sum of abs diff of P pairs.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+
+// You are given an array fo N integers (1 <= N <= 2e5)
+// You need to find P pairs (P <= N/2) such that the sum of the abs diff of pair is min
+
+void solve() {
+    int n, p; cin >> n >> p;
+    vlli a(n+1); for (int i = 1; i <= n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    vector<vlli> dp(n+1, vlli(n+1, INF));
+    for (int i = 0; i <= n; i++) dp[0][i] = 0;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= (i/2); j++) {
+            dp[i][j] = min(dp[i-1][j], a[i]-a[i-1] + dp[i-2][j-1]);
+        }
+    }
+    cout << dp[n][p] << endl;
+}   
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
