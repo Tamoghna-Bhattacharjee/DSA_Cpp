c1189bbb0b02921ed99da78662338b8d20aa8576
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-09-24 00:58:27 +0530

    Permutation Restoration => two pointer cf pb

---
+++ Array/Two pointer/PermutationRestoration.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+
+// https://codeforces.com/problemset/problem/1701/D
+
+void solve() {
+    int n; cin >> n;
+    vi b(n+1); for (int i = 1; i <= n; i++) cin >> b[i];
+    vi a(n+1);
+    map<int, vector<pair<int, int>>> mp;
+    for (int i = 1; i <= n; i++) {
+        int L = i/(b[i]+1) + 1;
+        int R = b[i] == 0? n: i/b[i];
+        mp[L].pb({R, i});
+    }
+    set<pair<int, int>> s;
+    for (int i = 1; i <= n; i++) {
+        for (auto j: mp[i]) s.insert(j);
+        auto it = *s.begin();
+        a[it.second] = i;
+        s.erase(s.begin());
+    }
+    for (int i = 1; i <= n; i++) cout << a[i] << " "; cout << endl;
+}   
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
