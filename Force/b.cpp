0f9a9d126cb68f8234e0bfd6ed29b67e40bebec5
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-09-24 15:25:55 +0530

    add

--- Force/b.cpp
+++ Force/b.cpp
@@ -4,164 +4,133 @@
 // using namespace __gnu_pbds;
 using namespace std;
 
-#define lli long long int 
-#define ld long double
-#define vi vector<int>
-#define vlli vector<lli>
-#define vpii vector<pair<int, int>>
-#define pb push_back 
+string getString(char ch[]) {
+    string s;
+    for (int i = 0; ch[i] != '\0'; i++) s.push_back(ch[i]);
+    return s;
+}
 
-template<typename T> void debug(T _a) {cout << _a << " ";}
-template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
-template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
-template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
-
-// typedef tree<int, null_type, less<int>, 
-//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-
-const lli INF = 1e18, MOD = 1e9 + 7;
-const int N = 2e5;
-const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
-const string YN[] = {"NO", "YES"};
-
-struct node {
-    lli ans;
-    node() {
-        // change
-        ans = 0;
+struct file {
+    string name, content;
+    int created, edited;
+    file() {}
+    file(string _name, string _content, int _created) {
+        name = _name;
+        content = _content;
+        created = _created;
+        edited = _created;
     }
-    node(lli x) {
-        // change
-        ans = x;
-    }
-    void merge(node &l, node &r) {
-        // change
-        ans = l.ans + r.ans;
+    file(string _name, string _content, int _created, int _edited) {
+        name = _name;
+        content = _content;
+        created = _created;
+        edited = _edited;
     }
 };
 
-struct update {
-    lli v;
-    update() {
-        // chnage
-        v = 0;
-    }
-    update(lli x) {
-        // chnage
-        v = x;
-    }
-    void combine(update &other) {
-        // change
-        v ^= other.v;
-    }
-    void apply(node &a, int ss, int se) {
-        // change
-        if (v == 1) a.ans = (se-ss+1) - a.ans;
-    }
-    operator==(update &other) {
-        // change
-        return v == other.v;
-    } 
-};
+struct Branch {
+    string name;
+    deque<Branch*> child;
+    deque<file*> files;
+    map<string, file*> mf;
+    Branch() {}
+    Branch(string _name) {name = _name;}
 
-struct segTree {
-    lli size;
-    vector<node> st;
-    vector<update> lazy;
-    node identity;
-    update identity_update;
-
-    void init(int x) {
-        size = x;
-        st.resize(4*size+4);
-        lazy.resize(4*size+4);
-        identity = node(); // change
-        identity_update = update(); // change
+    void removeOldestFile() {
+        file* f = files.front();
+        files.pop_front();
+        mf.erase(f->name);
     }
 
-    void pushdown(int si, int ss, int se) {
-        if (lazy[si] == identity_update) return;
-        lazy[si].apply(st[si], ss, se);
-        if (ss != se) {
-            lazy[2*si].combine(lazy[si]);
-            lazy[2*si+1].combine(lazy[si]);
+    void addFile(file* f) {
+        if (files.size() == 50) removeOldestFile();
+        mf[f->name] = f;
+        files.push_back(f);
+    }
+
+    void editFile(string fname, string data, int t) {
+        if (mf.find(fname) != mf.end()) {   
+            mf[fname]->content = data;
+            mf[fname]->edited = t;
         }
-        lazy[si] = identity_update;
-    }
-
-    void build (int si, int ss, int se, vlli &a) {
-        if (ss > se) return;
-        if (ss == se) {
-            st[si] = node(a[ss]); // change
-            return;
-        }
-        int mid = (ss + se) / 2;
-        build (2*si, ss, mid, a);
-        build (2*si + 1, mid + 1, se, a);
-        st[si].merge(st[2*si], st[2*si+1]);
-    }
-
-    void rupd(int si, int ss, int se, int us, int ue, update &upd) {
-        pushdown(si, ss, se);
-        if (us > se || ue < ss || ss > se) return;
-        if (ss >= us && se <= ue) { 
-            lazy[si].combine(upd);
-            pushdown(si, ss, se);
-            return;
-        }
-        int mid = (ss + se) / 2;
-        rupd(2*si, ss, mid, us, ue, upd);
-        rupd(2*si + 1, mid + 1, se, us, ue, upd);
-        st[si].merge(st[2*si], st[2*si + 1]);
-    }
-
-    node qry (int si, int ss, int se, int qs, int qe) {
-        pushdown(si, ss, se);
-        if (qs > se || qe < ss || ss > se) return identity;
-        if (ss >= qs && se <= qe) return st[si];
-        int mid = (ss + se)/2;
-        node l = qry(2*si, ss, mid, qs, qe);
-        node r = qry(2*si+1, mid+1, se, qs, qe);
-        node res; res.merge(l,r);
-        return res;
-    }
-
-    // driver code:
-    void build (vlli &a) {
-        // a must be 1-indexed
-        build(1, 1, size, a);
-    }
-
-    void rupd(int us, int ue, lli v) {
-        if (us > ue) return;
-        update upd(v);
-        rupd(1, 1, size, us, ue, upd);
-    }
-
-    node qry (int qs, int qe) {
-        if (qs > qe) return identity;
-        return qry (1, 1, size, qs, qe);
     }
 };
 
-void solve() {
-    
-}   
-  
+Branch* root;
+map<string, Branch*> mp;
+
+void init() {
+    root = new Branch("root");
+    for (auto i: mp) {
+        for (file* j: i.second->files) delete j;
+        delete i.second;
+    }
+    mp.clear();
+    mp["root"] = root;
+}
+
+void create(int t, char mb[], char mf[], char md[]) {
+    string bn = getString(mb), fn = getString(mf), con = getString(md);
+    file* f = new file(fn, con, t);
+    Branch* b = mp[bn];
+    b->addFile(f);
+}
+
+void edit(int t, char mb[], char mf[], char md[]) {
+    string bn = getString(mb), fn = getString(mf), con = getString(md);
+    Branch* b = mp[bn];
+    b->editFile(fn, con, t);
+}
+
+void branch(int t, char mb[], char mc[]) {
+    string bn = getString(mb), cn = getString(mc);
+    Branch* pb = mp[bn];
+    Branch* cb = new Branch(cn);
+    mp[cn] = cb;
+    for (auto i: pb->files) {
+        cb->files.push_back(new file(i->name, i->content, i->created, i->edited));
+    }
+    pb->child.push_back(cb);
+}
+
+void dfs(Branch* u) {
+    if (u->child.empty()) return;
+    for (Branch* v: u->child) {
+        dfs(v);
+        for (auto i: v->files) {
+            if (u->mf.find(i->name) == u->mf.end()) 
+                u->addFile(new file(i->name, i->content, i->created, i->edited));
+            else {
+                file* ff = u->mf[i->name];
+                if (ff->created == i->created && i->edited > ff->edited) {
+                    ff->content = i->content;
+                    ff->edited = i->edited;
+                }
+            }
+            v->mf.erase(i->name);
+            delete i;
+        }
+        mp.erase(v->name);
+        delete v;
+    }
+    u->child.clear();
+}
+
+void merge(int t, char mb[]) {
+    string bn = getString(mb);
+    Branch* b = mp[bn];
+    dfs(b);
+}
+
+int readFile(int t, char mb[], char mf[], char retString[]) {
+    string bn = getString(mb), fn = getString(mf);
+    Branch* b = mp[bn];
+    string res = b->mf[fn]->content;
+    for (int i = 0; i < res.size(); i++) retString[i] = res[i];
+    retString[res.size()] = '\0';
+    return res.size();
+}
+
 int main() {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(0);
-    cout << fixed << setprecision(9);
-    int t = 1; //cin >> t;
-    for (int _i = 1; _i <= t; _i++) {
-        //cout << "Case #" << _i << ": ";
-        solve();
-    }
+    
 }
