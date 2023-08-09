096ce9793652c02ad9c08d87167360169be73306
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-08-10 00:40:06 +0530

    restructure + new problem

---
+++ SDE/dp/Min time to make sum less than x.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/minimum-time-to-make-array-sum-at-most-x/description/
+
+void solve() {
+    int n, x; cin >> n >> x;
+    vi a(n+1), b(n+1);
+    
+    int sa = 0, sb = 0;
+    for (int i = 1; i <= n; i++) cin >> a[i], sa += a[i];
+    for (int i = 1; i <= n; i++) cin >> b[i], sb += b[i];
+    
+    vpii v(n+1);
+    for (int i = 1; i <= n; i++) v[i] = {b[i], a[i]};
+    sort(v.begin() + 1, v.end());
+    
+    int dp[n+1][n+1]; 
+    // dp[i][j] -> max value i can remove with i element in j times
+    memset(dp, 0, sizeof dp);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] + v[i].second + j * v[i].first);
+        }
+    }
+
+    for (int i = 0; i <= n; i++) {
+        if (sa + i*sb - dp[n][i] <= x) {
+            cout << i << endl;
+            return;
+        }
+    }
+    cout << -1 << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
