9e66c7abc3a520d415be01528970aeb248676ef9
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-08-13 14:56:49 +0530

    K-th element of two sorted Arrays

---
+++ SDE/Array/Binary Search/K-th element of two sorted Arrays.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+int kthElement(vi &a, vi &b, int k) {
+    int n = a.size(), m = b.size();
+    if (n == 0) return b[k-1];
+    if (m == 0) return a[k-1];
+    int L = max(0, k-m), R = min(n, k);
+    while (L <= R) {
+        int mid = (L + R) >> 1;
+        int sza = mid;
+        int szb = k-mid;
+        int la = sza > 0? a[sza-1]: INT_MIN;
+        int lb = szb > 0? b[szb-1]: INT_MIN;
+        int ra = sza < n? a[sza]: INT_MAX;
+        int rb = szb < m? b[szb]: INT_MAX;
+        
+        if (la <= rb && lb <= ra) return max(la, lb);
+        else if (rb < la) R = mid - 1;
+        else L = mid + 1;
+    }
+    return 0;
+}
+
+void solve() {
+    int n, m, k; cin >> n >> m >> k;
+    vi a(n), b(m); 
+    for (auto &i: a) cin >> i;
+    for (auto &i: b) cin >> i;
+    cout << kthElement(a, b, k) << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
