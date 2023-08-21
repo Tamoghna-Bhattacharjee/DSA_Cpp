821d5ce38f20fc27d7a1668955bebfdb4bcf847d
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-08-22 00:58:54 +0530

    KMP problems

---
+++ String/KMP and LPS/1. KMP template.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// Finding a pattern inside a text using KMP algo
+
+vi getLPS(string &s) {
+    int n = s.size();
+    int i = 1, j = 0;
+    vi LPS(n);
+    while (i < n) {
+        if (s[i] == s[j]) LPS[i++] = ++j;
+        else if (j == 0) i++;
+        else j = LPS[j-1];
+    }
+    return LPS;
+}
+
+bool isSubstr(string &t, string &p, vi &LPS) {
+    int n = t.size(), m = p.size();
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (t[i] == p[j]) {
+            i++; j++;
+            continue;
+        } else {
+            if (j == 0) i++;
+            else j = LPS[j-1];
+        }
+    }
+    return j == m;
+}
+
+void solve() {
+    string pattern, text; cin >> pattern >> text;
+    vi LPS = getLPS(pattern);
+    cout << YN[isSubstr(text, pattern, LPS)] << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
