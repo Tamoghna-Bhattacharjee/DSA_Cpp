074da57cb94b8fd0c3a40c82fa886276e138365c
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-08-26 18:25:01 +0530

    New knapsac problem -> good observational on how to use knapsack

---
+++ Dynamic Programming/Knapsac/Magic Will Save the World.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://codeforces.com/contest/1862/problem/F
+
+void solve() {
+    lli w, f, n; cin >> w >> f >> n;
+    lli sum = 0;
+    vlli a(n+1); for (int i = 1; i <= n; i++) cin >> a[i], sum += a[i];
+    
+    vector<vi> dp(2, vi(sum + 1));
+    dp[0][0] = 1;
+    for (lli i = 1; i <= n; i++) {
+        for (lli j = 0; j <= sum; j++) {
+            dp[1][j] = dp[0][j];
+            if (j-a[i] >= 0) dp[1][j] = dp[1][j] || dp[0][j-a[i]];
+        }
+        dp[0] = dp[1];
+    }
+    lli ans = 2e9;
+    for (int i = 0; i <= sum; ++i) {
+        if (dp[1][i]) {
+            ans = min(ans, max((i + w - 1) / w, (sum - i + f - 1) / f));
+        }
+    }
+    cout << ans << "\n";
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
