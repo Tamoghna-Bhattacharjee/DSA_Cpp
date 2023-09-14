5c259f251858a0f52f4e378d16ef51abc6c3b484
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-09-14 22:27:16 +0530

    Reconstruct Itinerary

---
+++ SDE/Graph/Dfs/Reconstruct Itinerary.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/reconstruct-itinerary/
+
+map<string, vector<string>> g;
+vector<string> ans;
+
+void dfs(string u) {
+  while (!g[u].empty()) {
+      string v = g[u].back(); g[u].pop_back();
+      dfs(v);
+  }
+  ans.push_back(u);
+}
+
+void solve() {
+    int n; cin >> n;
+    ans.clear();
+    g.clear();
+    for (int i = 0; i < n; i++) {
+        string u, v; cin >> u >> v;
+        g[u].push_back(v);
+    }
+    for (auto &i: g) {
+        sort(i.second.begin(), i.second.end());
+        reverse(i.second.begin(), i.second.end());
+    }
+    dfs("JFK");
+    reverse(ans.begin(), ans.end());
+    debug(ans);
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; // cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
