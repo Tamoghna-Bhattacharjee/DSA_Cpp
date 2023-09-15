808415e58615f4547b8b72d87d5d2062abfed3b1
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-09-16 00:41:10 +0530

    Min Cost to Connect All Points

---
+++ SDE/Dsu/Min Cost to Connect All Points.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+int findNode(int a, vi &dsu) {
+    if (dsu[a] < 0) return a;
+    return dsu[a] = findNode(dsu[a], dsu);
+}
+
+bool unionNode(int a, int b, vi &dsu) {
+    a = findNode(a, dsu); b = findNode(b, dsu);
+    if (a == b) return false;
+    if (dsu[a] <= dsu[b]) swap(a, b);
+    dsu[b] += dsu[a];
+    dsu[a] = b;
+    return true;
+}
+
+int getCost(pair<int, int> &a, pair<int, int> &b) {
+    return abs(a.first-b.first) + abs(a.second-b.second);
+}
+
+void solve() {
+    int n; cin >> n;
+    vpii a(n); for (auto &i: a) cin >> i.first >> i.second;
+    if (n == 1) {cout << 0 << endl; return;}
+    vector<pair<int, pair<int, int>>> edge;
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            edge.push_back({getCost(a[i], a[j]), {i, j}});
+        }
+    }
+    sort(edge.begin(), edge.end());
+    vi dsu(n, -1);
+    int ans = 0;
+    for (auto &i: edge) {
+        int val = i.first, u = i.second.first, v = i.second.second;
+        if (unionNode(u, v, dsu)) ans += val;
+    }
+    cout << ans << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
