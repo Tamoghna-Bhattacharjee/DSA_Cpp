57550b487ca4af1102b485d03fa2eb0d3aae1a1f
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-09-17 00:05:49 +0530

    Cnt pair od points with k dist

---
+++ SDE/dp/Cnt Pair of points with dist K.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/count-pairs-of-points-with-distance-k/
+// Find pair such that (x1^x2) + (y1^y2) = k;
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vpii a(n); for (auto &i: a) cin >> i.first >> i.second;
+    map<pair<int, int>, int> dp;
+    int ans = 0;
+    for (auto &i: a) {
+        int x1 = i.first, y1 = i.second;
+        for (int X = 0; X <= k; X++) {
+            int Y = k-X;
+            int x2 = X^x1, y2 = Y^y1;
+            ans += dp[{x2, y2}];
+        }
+        dp[{x1, y1}]++;
+    }
+    cout << ans << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; //cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
