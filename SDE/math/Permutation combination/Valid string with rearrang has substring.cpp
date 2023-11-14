d262564a4cbd318f0eb8e57844fd17ffd23faba3
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-11-14 11:58:24 +0530

    new permutation combination problems

---
+++ SDE/math/Permutation combination/Valid string with rearrang has substring.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/number-of-strings-which-can-be-rearranged-to-contain-substring/
+
+
+lli power(lli a, lli n) {
+    lli res = 1;
+    while (n) {
+        if (n & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        n /= 2;
+    }
+    return res % MOD;
+}
+
+void solve() {
+    int n; cin >> n;
+    lli ans = power(26, n) % MOD;
+    ans = (ans - (n+75)*power(25, n-1) % MOD + MOD) % MOD;
+    ans = (ans + (72 + 2*n) * power(24,n-1) % MOD) % MOD;
+    ans = (ans - (23+n) * power(23,n-1) % MOD + MOD) % MOD;
+    return ans;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
