44ec1fd29476e031deeb1fa34b00220f3fab6be5
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-12-23 12:13:50 +0530

    Score sum

---
+++ SDE/math/Permutation combination/Score Sum.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 998244353;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://www.codechef.com/problems/SCORESUM7
+// idea -> for every pair (i,j) with 1 < i < j <= n the contribution of that pair is 0
+// this is because we can use pair {i,j} with both +ve and -ve len sub seq using elements
+// from 1 to i-1
+
+lli power(lli a, lli n) {
+    lli res = 1;
+    while (n > 0) {
+        if (n % 2) res = res * a % MOD;
+        a = a * a % MOD;
+        n = n/2;
+    }
+    return res;
+}
+
+void solve() {
+    int n; cin >> n;
+    vlli a(n+1); for (int i = 1; i <= n; i++) cin >> a[i];
+    lli ans = 0;
+    for (int i = 2; i <= n; i++) {
+        ans += power(abs(a[i]-a[1]), 7) % MOD * power(2, n-i) % MOD;
+        ans %= MOD;
+    }
+    cout << ans << endl;
+}
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
