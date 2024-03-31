9729173d176ae2780a23e5aa6762ae78504f9ccc
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2024-03-31 10:10:17 +0530

    Minimize Manhattan Distances -- Leetcode -- HARD

---
+++ SDE/math/Minimize Manhattan Distances.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back 
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/minimize-manhattan-distances/
+// https://www.geeksforgeeks.org/maximum-manhattan-distance-between-a-distinct-pair-from-n-coordinates/
+
+/* |Xi – Xj| + |Yi – Yj| = max((Xi – Yi) – (Xj – Yj), 
+                                (-Xi + Yi) – (-Xj + Yj), 
+                                (-Xi – Yi) – (-Xj – Yj), 
+                                (Xi + Yi) – (Xj + Yj))
+*/
+
+void solve() {
+    int n; cin >> n;
+    vector<vi> p(n, vi(2));
+    for (auto &i: p) cin >> i[0] >> i[1];
+    multiset<int> sum, diff;
+    for (auto &i: p) {
+        sum.insert(i[0]+i[1]);
+        diff.insert(i[0]-i[1]);
+    }
+    int ans = INT_MAX;
+    for (auto &i: p) {
+        sum.erase(sum.find(i[0] + i[1]));
+        diff.erase(diff.find(i[0] - i[1]));
+        ans = min(ans, max((*sum.rbegin()) - (*sum.begin()), 
+                            (*diff.rbegin()) - (*diff.begin())));
+        sum.insert(i[0]+i[1]);
+        diff.insert(i[0]-i[1]);
+    }
+    cout << ans << endl;
+}
+
+class Solution {
+public:
+    int minimumDistance(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<int> cand = {0, 1, n - 2, n - 1};
+        vector<int> xs(n), ys(n);
+        for (int i = 0; i < n; ++i) {
+            xs[i] = points[i][0] + points[i][1] + i;
+            ys[i] = points[i][0] - points[i][1] + i;
+        }
+        
+        sort(xs.begin(), xs.end());
+        sort(ys.begin(), ys.end());
+        int res = INT_MAX;
+        for (int c : cand) {
+            for (int d : cand) {
+                int cur = 0;
+                cur = max(cur, abs(points[c][0] + points[c][1] - xs[d] + d));
+                cur = max(cur, abs(points[c][0] - points[c][1] - ys[d] + d));
+                res = min(res, cur);
+            }
+        }
+        return res;
+    }
+};
+  
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
