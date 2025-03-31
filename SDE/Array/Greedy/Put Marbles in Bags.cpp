80de3a87389d88098732e0b49b9c5d3aad570b54
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2025-03-31 21:46:54 +0530

    2551. Put Marbles in Bags

---
+++ SDE/Array/Greedy/Put Marbles in Bags.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back
+#define all(__x) __x.begin(),__x.end()
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/put-marbles-in-bags/
+
+// idea is if we want to split a array in k subarr we need k-1 splits
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n); for (auto &i: a) cin >> i;
+    vi v;
+    for (int i = 0; i+1 < n; i++) v.pb(a[i]+a[i+1]);
+    sort(all(v));
+    lli mi = 0, mx = 0;
+    for (int i = 0; i < k-1; i++) mi += v[i];
+    for (int i = v.size()-1, kk = k-1; i >= 0 && kk > 0; i--, kk--) {
+        mx += v[i];
+    }
+    
+    cout << mx - mi << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
