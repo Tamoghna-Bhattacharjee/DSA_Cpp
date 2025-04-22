57bd83e30064012f2344fed26f86519d3f1037f1
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2025-04-22 23:01:38 +0530

    2338. Count the Number of Ideal Arrays

---
+++ SDE/math/Permutation combination/Cnt the num of Ideal Arrays.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back
+#define all(__x) __x.begin(),__x.end()
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 1e4;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/count-the-number-of-ideal-arrays/
+// https://www.youtube.com/watch?v=rwPXJnuqon8
+// Idea is to work with unique element, max unique element = log2(n)
+
+lli F1[N+1], F2[N+1], inv[N+1];
+
+void pre() {
+    inv[1] = 1;
+    for (int i = 2; i <= N; i++) {
+        inv[i] = (MOD - (MOD/i * inv[MOD%i]) % MOD) % MOD;
+    }
+    F1[0] = F2[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        F1[i] = F1[i-1] * i % MOD;
+        F2[i] = F2[i-1] * inv[i] % MOD;
+    }
+}
+
+lli ncr(int n, int r) {
+    if (r > n) return 0;
+    return F1[n] % MOD * F2[r] % MOD * F2[n-r] % MOD;
+}
+
+void solve() {
+    int n, mx; cin >> n >> mx;
+    vector<vlli> dp(mx + 1, vlli(15));
+    // dp[i][k] = num of arrays of unique elements ending with i having len k
+    for (int i = 1; i <= mx; i++) {
+        dp[i][1] = 1;
+        for (int j = i+i; j <= mx; j+= i) {
+            for (int k = 2; k <= min(n, 14); k++) {
+                (dp[j][k] += dp[i][k-1]) %= MOD;
+            }
+        }
+    }
+    
+    lli ans = 0;
+    for (int i = 1; i <= mx; i++) {
+        for (int k = 1; k <= min(n, 14); k++) {
+            (ans += dp[i][k] * ncr(n-1, k-1) % MOD) %= MOD;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    pre();
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
