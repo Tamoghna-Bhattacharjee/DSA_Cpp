deae2b0fefff90422bd517247809a80d970e05f9
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2025-06-05 23:43:52 +0530

    3403. Find the Lexicographically Largest String From the Box I

---
+++ SDE/String/Sliding window/Lexiographic largest substr.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back
+#define all(__x) __x.begin(),__x.end()
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/find-the-lexicographically-largest-string-from-the-box-i
+// https://leetcode.com/problems/last-substring-in-lexicographical-order/
+
+void solve() {
+    string s; cin >> s;
+    int cnt; cin >> cnt;
+    int n = s.size();
+
+    int mxlen = n-cnt+1;
+    int i = 0, j = 1;
+
+    while (j < n) {
+        int k = 0;
+        while (j+k < n && s[i+k] == s[j+k]) k++;
+        if (j+k < n) {
+            if (s[i+k] < s[j+k]) {
+                int tmp = i;
+                i = j;
+                j = max(i+1, tmp+k+1);
+            } else j += k+1;
+        } else j += k+1;
+    }
+
+    cout << s.substr(i, min(n-i, mxlen)) << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
