8ba56f02fcedc271b975a73251d83100c082dd15
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2025-11-09 20:26:06 +0530

    2014. Longest Subsequence Repeated k Times

---
+++ SDE/Graph/Bfs/Long subseq rep k times.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+#define lli long long int 
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vpii vector<pair<int, int>>
+#define pb push_back
+#define all(__x) __x.begin(),__x.end()
+
+template<typename T> void debug(T _a) {cout << _a << " ";}
+template<typename T1, typename T2> void debug(pair<T1, T2> _p) {cout<<"{";debug(_p.first);cout<<": ";debug(_p.second);cout<<"}\n";}
+template<typename T> void debug(vector<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(deque<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(multiset<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T> void debug(set<T> _aa) {for (auto h: _aa) debug(h); cout << endl;}
+template<typename T1, typename T2> void debug(map<T1, T2> _mm) {for (auto h: _mm) debug(h);}
+
+// typedef tree<int, null_type, less<int>, 
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+const lli INF = 1e18, MOD = 1e9 + 7;
+const int N = 2e5;
+const int di[] = {-1,0,1,0}, dj[] = {0,1,0,-1};
+const string YN[] = {"NO", "YES"};
+const double PI = acos(-1);
+
+// https://leetcode.com/problems/longest-subsequence-repeated-k-times
+// max len of possible sub seq is 8 since n < (2000, 8*k) 
+// The total number of possible valid subsequences of all lengths is 
+// C(8, 1) + C(8, 2) + ... + C(8, 7) ~= 2 ^ 8 = 256
+
+bool repKtimes(string &tmp, string &s, int k) {
+    int i = 0, j = 0, cnt = 0;
+    while (j < s.size()) {
+        if (tmp[i] == s[j]) i++;
+        if (i == tmp.size()) i = 0, cnt++;
+        j++;
+    }
+    return cnt >= k;
+}
+
+void solve() {
+    string s; cin >> s;
+    int k; cin >> k;
+    int n = s.size();
+
+    vi F(26);
+    for (char i: s) F[i-'a']++;
+
+    vector<string> can;
+    for (int i = 25; i >= 0; i--) {
+        if (F[i] >= k) can.pb(string(1, i+'a'));
+    }
+
+    queue<string> q;
+    string ans;
+    for (auto &i: can) q.push(i);
+
+     while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        if (cur.size() > ans.size()) ans = cur;
+        for (auto &i: can) {
+            string tmp = cur + i;
+            if (repKtimes(tmp, s, k)) q.push(tmp);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    cout << fixed << setprecision(9);
+    int t = 1; cin >> t;
+    for (int _i = 1; _i <= t; _i++) {
+        //cout << "Case #" << _i << ": ";
+        solve();
+    }
+}
